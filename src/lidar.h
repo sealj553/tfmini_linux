869e738c0c2aa6fc1b1a466ea50928ccf5c7b853
Jackson <sealj553@gmail.com>
2018-05-02 18:51:24 -0400

    added comments

--- src/lidar.h
+++ src/lidar.h
@@ -7,8 +7,11 @@ typedef struct lidar {
     uint16_t dist, stren;
 } Lidar;
 
+//creates a Lidar that reads from tty
 Lidar* lidar_create(const char *tty);
 
+//if 1 is returned, a measurement was sucessfully read, otherwise 0 is returned
 int lidar_scan(Lidar *l);
 
+//closes and flushes serial port 
 void lidar_close(Lidar *l);
